eb81720026b70132c267968c3d3a8ea34efadd6b
Jonathan Rugama <jrugama96@gmail.com>
2025-11-29 22:02:29 -0600

    Example: With this example I was trying to understand recursivity

--- project-2/js/app.js
+++ project-2/js/app.js
@@ -24,21 +24,42 @@ function createTurnOnOffButton() {
 
 
 function main() {
-    let colorPattern1 = ["red", "green", "red", "green", "red", "green", "red", "green", "red", "green"];
-    let colorPattern2 = ["blue", "yellow", "blue", "yellow", "blue", "yellow", "blue", "yellow", "blue", "yellow"];
-    let colorPattern3 = ["green", "green", "red", "red", "green", "green", "red", "red", "green", "green"];
-    let colorPattern4 = ["yellow", "blue", "red", "yellow", "blue", "red", "yellow", "blue", "red", "yellow"];
-    let colorPattern5 = ["red", "red", "red", "red", "red", "green", "green", "green", "green", "green"];
+    let patterns = [
+        ["red", "green", "red", "green", "red", "green", "red", "green", "red", "green"],
+        ["blue", "yellow", "blue", "yellow", "blue", "yellow", "blue", "yellow", "blue", "yellow"],
+        ["green", "green", "red", "red", "green", "green", "red", "red", "green", "green"],
+        ["yellow", "blue", "red", "yellow", "blue", "red", "yellow", "blue", "red", "yellow"],
+        ["red", "red", "red", "red", "red", "green", "green", "green", "green", "green"]
+    ];
+
 
     for (let i = 0; i < 10; i++) {
         createLight();
     }
+    let lights = document.querySelectorAll(".light");
     let button = createTurnOnOffButton();
-
+    let contador = 0;
     button.addEventListener("click", () => {
         if (button.textContent === "Turn on") {
             button.textContent = "Turn off";
+            function cuentaRegresiva(segundos) {
+                // 1. Caso Base (Condición de salida)
+                // Si no detenemos la recursividad aquí, se ejecutaría infinitamente.
+                if (segundos < 0) {
+                    console.log("¡Tiempo terminado!");
+                    return;
+                }
 
+                // 2. Acción actual
+                console.log(segundos);
+
+                // 3. Llamada Recursiva con Retraso
+                // Llamamos a la misma función, pero con (n - 1) y esperamos 1000ms (1s).
+                setTimeout(function () {
+                    cuentaRegresiva(segundos - 1);
+                }, 1000);
+            }
+            cuentaRegresiva(10);
         } else {
             button.textContent = "Turn on";
         }
